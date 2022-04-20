85cf9b4b65749e75e13d00231e6a7f5de3cb366f
cha <cha.git@mailo.fr>
2022-04-20 11:20:45 +0200

    Add stats module

---
+++ src/stats.ml
@@ -0,0 +1,37 @@
+type t = {
+  mutable miss : int;
+  mutable hit : int;
+  mutable replace : int;
+  mutable remove : int;
+  mutable clear : int;
+  mutable add : int;
+  mutable max_size : int;
+  mutable current : int;
+}
+
+let v =
+  {
+    miss = 0;
+    hit = 0;
+    replace = 0;
+    remove = 0;
+    clear = 0;
+    add = 0;
+    max_size = 0;
+    current = 0;
+  }
+
+let miss t = t.miss <- t.miss + 1
+let replace t = t.replace <- t.replace + 1
+
+let remove t =
+  t.remove <- t.remove + 1;
+  t.current <- t.current - 1
+
+let clear t = t.clear <- t.clear + 1
+let max_size t v = if t.max_size < v then t.max_size <- v
+
+let add t =
+  t.add <- t.add + 1;
+  t.current <- t.current + 1;
+  max_size t t.current
---
+++ src/stats.mli
@@ -0,0 +1,61 @@
+type t = private {
+  mutable miss : int;
+  mutable hit : int;
+  mutable replace : int;
+  mutable remove : int;
+  mutable clear : int;
+  mutable add : int;
+  mutable max_size : int;
+  mutable current : int;
+}
+
+val v : unit -> t
+(*@ t = v ()
+  ensures t.miss = 0
+  ensures t.hit = 0
+  ensures t.replace = 0
+  ensures t.remove = 0
+  ensures t.clear = 0
+  ensures t.add = 0
+  ensures t.max_size = 0
+  ensures t.current = 0
+*)
+
+val miss : t -> unit
+(*@ miss t
+    modifies t.miss
+    ensures  t.miss = old t.miss + 1
+*)
+
+val hit : t -> unit
+(*@ hit t
+    modifies t.hit
+    ensures  t.hit = old t.hit + 1
+*)
+
+val replace : t -> unit
+(*@ replace t
+    modifies t.replace
+    ensures  t.replace = old t.replace + 1
+*)
+
+val remove : t -> unit
+(*@ remove t
+    modifies t.remove, t.current
+    ensures  t.remove  = old t.remove + 1
+    ensures  t.current = old t.current - 1
+*)
+
+val clear : t -> unit
+(*@ clear t
+    modifies t.clear
+    ensures  t.clear = old t.clear + 1
+*)
+
+val add : t -> unit
+(*@ add t
+  modifies t.current, t.max_size, t.add
+  ensures  t.add = old t.add + 1
+  ensures  t.current = old t.current + 1
+  ensures t.max_size >= old t.max_size
+*)
