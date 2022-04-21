5c700136eea234552d631dbff69a0a5d6849d1b2
cha <cha.git@mailo.fr>
2022-04-21 14:25:48 +0200

    add stats counters in LRU file

--- src/lru.ml
+++ src/lru.ml
@@ -9,9 +9,20 @@ struct
 
   let dummy : K.t = Obj.magic (ref 0)
 
-  type 'a t = { tbl : (int * 'a) H.t; lst : K.t Dllist.t; cap : int }
+  type 'a t = {
+    tbl : (int * 'a) H.t;
+    lst : K.t Dllist.t;
+    cap : int;
+    stats : Stats.t;
+  }
 
-  let unsafe_v c = { tbl = H.create c; lst = Dllist.create c dummy; cap = c }
+  let unsafe_v c =
+    {
+      tbl = H.create c;
+      lst = Dllist.create c dummy;
+      cap = c;
+      stats = Stats.v ();
+    }
 
   let v c =
     if c <= 0 then invalid_arg "capacity must be strictly positive";
@@ -23,7 +34,8 @@ struct
 
   let clear t =
     H.clear t.tbl;
-    Dllist.clear t.lst
+    Dllist.clear t.lst;
+    Stats.clear t.stats
 
   let mem t k = H.mem t.tbl k
 
@@ -36,8 +48,11 @@ struct
   let find_opt t k =
     try
       let result = find t k in
+      Stats.hit t.stats;
       Some result
-    with Not_found -> None
+    with Not_found ->
+      Stats.miss t.stats;
+      None
 
   let promote t k = ignore (find t k)
 
@@ -45,16 +60,19 @@ struct
     try
       let index, _value = H.find t.tbl k in
       let new_index = Dllist.promote t.lst index in
-      H.replace t.tbl k (new_index, v)
+      H.replace t.tbl k (new_index, v);
+      Stats.replace t.stats
     with Not_found ->
       let index, removed = Dllist.append t.lst k in
       (match removed with None -> () | Some key -> H.remove t.tbl key);
-      H.replace t.tbl k (index, v)
+      H.replace t.tbl k (index, v);
+      Stats.add t.stats
 
   let remove t k =
     try
       let index, _value = H.find t.tbl k in
       Dllist.remove t.lst index;
-      H.remove t.tbl k
+      H.remove t.tbl k;
+      Stats.remove t.stats
     with Not_found -> ()
 end
