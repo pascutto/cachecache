a1d9fc3c56f3a5dfdbe92b3b1a360cafbd49356e
cha <cha.git@mailo.fr>
2022-04-25 11:04:48 +0200

    start to implement through functor

--- src/s.ml
+++ src/s.ml
@@ -1,5 +1,6 @@
 module type Cache = sig
   type 'a t
+  type k
 
   val v : int -> 'a t
   val stats : 'a t -> Stats.t
@@ -7,19 +8,20 @@ module type Cache = sig
   val capacity : 'a t -> int
   val size : 'a t -> int
   val clear : 'a t -> unit
-  (*val mem : 'a t -> K.t -> bool
-    val find : 'a t -> K.t -> 'a
-    val promote : 'a t -> K.t -> unit
-    val find_opt : 'a t -> K.t -> 'a option
-    val replace : 'a t -> K.t -> 'a -> unit
-    val remove : 'a t -> K.t -> unit*)
+  val mem : 'a t -> k -> bool
+  val find : 'a t -> k -> 'a
+  val promote : 'a t -> k -> unit
+  val find_opt : 'a t -> k -> 'a option
+  val replace : 'a t -> k -> 'a -> unit
+  val remove : 'a t -> k -> unit
 end
 
-module type BD = sig
+module type DB = sig
   type k
   type v
 
   val mem : k -> v -> bool
   val find : k -> v
   val add : k -> v -> unit
+  val remove : k -> unit
 end
---
+++ src/through.ml
@@ -0,0 +1,67 @@
+module Make (
+  C : S.Cache
+  B : S.Db
+) = struct
+
+  type 'a t = {
+    tbl : (int * 'a) H.t;
+    lst : K.t Dllist.t;
+    cap : int;
+    stats : Stats.t;
+  }
+
+  let unsafe_v c = C.unsafe_v c
+  let v c = C.v c
+  let stats t = C.stats t
+  let is_empty t = C.is_empty t
+  let capacity t = C.capacity t
+  let size t = C.size t
+  let clear t = C.clear t
+
+  let mem t k =
+    let b = C.mem t k in
+    if b then b else B.mem t k
+    
+
+
+
+  (*let find t k = 
+    let index, value = H.find t.tbl k in
+    let new_index = Dllist.promote t.lst index in
+    H.replace t.tbl k (new_index, value);
+    value
+
+  let find_opt t k =
+    try
+      let result = find t k in
+      Stats.hit t.stats;
+      Some result
+    with Not_found ->
+      Stats.miss t.stats;
+      None
+
+  let promote t k = ignore (find t k)
+
+  let replace t k v =
+    try
+      let index, _value = H.find t.tbl k in
+      let new_index = Dllist.promote t.lst index in
+      Stats.replace t.stats;
+      H.replace t.tbl k (new_index, v)
+    with Not_found ->
+      let index, removed = Dllist.append t.lst k in
+      (match removed with
+      | None -> Stats.add (H.length t.tbl + 1) t.stats
+      | Some key ->
+          H.remove t.tbl key;
+          Stats.discard t.stats);
+      H.replace t.tbl k (index, v)
+      *)
+
+
+
+  let remove t k =
+    (*if (C.remove t k) == 0 then B.remove k*)
+    let c = C.remove t k in
+    if c == 0 then B.remove k
+end
