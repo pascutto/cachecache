e1d1d250fab5ea1192b07dd8e1ba13960034c956
Lucccyo <cha.git@mailo.fr>
2022-06-01 14:08:01 +0200

    r

--- bench/replay.ml
+++ bench/replay.ml
@@ -51,9 +51,7 @@ module Make (Cache : Cachecache.S.Cache with type key = K.t) = struct
         match op with
         | Add k ->
             let before = Mtime_clock.count counter in
-            for _ = 0 to 10 do
-              Cache.replace cache k ()
-            done;
+            Cache.replace cache k ();
             let after = Mtime_clock.count counter in
             stats.total_runtime_span <-
               Mtime.Span.(abs_diff after before |> add stats.total_runtime_span);
@@ -62,9 +60,7 @@ module Make (Cache : Cachecache.S.Cache with type key = K.t) = struct
             stats.add <- stats.add + 1
         | Find k ->
             let before = Mtime_clock.count counter in
-            for _ = 0 to 10 do
-              ignore (Cache.find_opt cache k : _ option)
-            done;
+            ignore (Cache.find_opt cache k : _ option);
             let after = Mtime_clock.count counter in
             stats.total_runtime_span <-
               Mtime.Span.(abs_diff after before |> add stats.total_runtime_span);
@@ -73,17 +69,14 @@ module Make (Cache : Cachecache.S.Cache with type key = K.t) = struct
             stats.find <- stats.find + 1
         | Mem k ->
             let before = Mtime_clock.count counter in
-            for _ = 0 to 10 do
-              let b = Cache.mem cache k in
-              if b then stats.hit <- stats.hit + 1
-              else stats.miss <- stats.miss + 1
-            done;
+            let b = Cache.mem cache k in
             let after = Mtime_clock.count counter in
             stats.total_runtime_span <-
               Mtime.Span.(abs_diff after before |> add stats.total_runtime_span);
             stats.mem_span <-
               Mtime.Span.(abs_diff after before |> add stats.mem_span);
-
+            if b then stats.hit <- stats.hit + 1
+            else stats.miss <- stats.miss + 1;
             stats.mem <- stats.mem + 1
         | _ -> assert false)
       seq;
